aaa7c1606b1426a323d8ffbe6236fd06c5ce8275
Ryo Matsumoto <e165738@ie.u-ryukyu.ac.jp>
2016-11-17 13:29:55 +0900

    Step2 cleared.

--- src/main/jp/ac/uryukyu/ie/e165738/Main.java
+++ src/main/jp/ac/uryukyu/ie/e165738/Main.java
@@ -12,7 +12,7 @@ public class Main {
             turn++;
             System.out.printf("%dターン目開始！\n", turn);
             hero.attack(enemy);
-            enemy.attack(hero);  // if追加
+            enemy.attack(hero);
         }
         System.out.println("戦闘終了");
     }
